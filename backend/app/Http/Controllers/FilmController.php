60ea365aab03db856f3048aa4696c8695d6ba68c
Andres Saez <andres14.ass@gmail.com>
2024-01-22 14:50:04 +0100

    refact(FilmController): refact methods

--- backend/app/Http/Controllers/FilmController.php
+++ backend/app/Http/Controllers/FilmController.php
@@ -9,37 +9,39 @@ use Illuminate\Http\Request;
 class FilmController extends Controller
 {
     public function showAll() {
-        $films = Film::all();
-        return response()->json($films);
+        try {
+            $films = Film::all();
+            return response()->json($films);
+        } catch (\Throwable $th) {
+            return response()->json('Error to show all films');
+        }
     }
 
     public function showOne($id) {
-        $film = Film::find($id);
-        return response()->json($film);
+        try {
+            $film = Film::find($id);
+            return response()->json($film);
+        } catch (\Throwable $th) {
+            return response()->json('Film not found');
+        }
     }
 
     public function showRandom() {
-        $films = Film::all()->random(7);
-        return response()->json($films);
+        try {
+            $films = Film::all()->random(7);
+            return response()->json($films);
+        } catch (\Throwable $th) {
+            return response()->json('Error to show random films');
+        }
     }
 
-    public function create(Request $request) {
-        /*$film = new Film();
-        $film->title = $request->title;
-        $film->description = $request->description;
-        $film->director = $request->director;
-        $film->year = $request->year;
-        $film->image = $request->image;
-        $film->duration = $request->duration;
-        $film->score = $request->score;
-        $film->id_category = $request->id_category;
-        $film->id_torrent = $request->id_torrent;
-        $film->save();
-        return response()->json($film);*/
-        
-        $film = Film::create($request->all());
-
-        return response()->json(['message' => 'Film created successfully', 'film' => $film], 201);
+    public function create(Request $request) {        
+        try {
+            $film = Film::create($request->all());
+            return response()->json(['message' => 'Film created successfully', 'film' => $film], 201);
+        } catch (\Throwable $th) {
+            return response()->json('Error to create film');
+        }
     }
 
     public function update(Request $request, $id) {
@@ -63,30 +65,20 @@ class FilmController extends Controller
     }
 
     public function search(Request $request) {
-        $films = Film::where('title', 'like', '%'.$request->search.'%')->get();
-        return response()->json($films);
+        try {
+            $films = Film::where('title', 'like', '%'.$request->search.'%')->get();
+            return response()->json($films);
+        } catch (\Throwable $th) {
+            return response()->json('Error to search films');
+        }
     }
 
     public function getByIdCategory($id) {
-        $films = Film::where('id_category', $id)->get();
-        return response()->json($films);
-    }
-
-    /*public function stream($id) {
-        $film = Film::find($id);
-        if ($film) {
-            $torrentID = $film->id_torrent;
-
-            if (!is_null($torrentId)) {
-                // Construir el comando para ejecutar Peerflix con el magnet link del torrent
-                $magnetLink = $film->torrent->magnet_link;
-        
-                $command = "node " . base_path("../ms-streamTorrent/stream.js");
-
-                // Ejecutar Peerflix como un proceso secundario
-                Artisan::call('process:run', ['command' => $command]);
-            }
+        try {
+            $films = Film::where('id_category', $id)->get();
+            return response()->json($films);
+        } catch (\Throwable $th) {
+            return response()->json('Error to get films by category');
         }
-        return response()->json($film);
-    }*/
+    }
 }
